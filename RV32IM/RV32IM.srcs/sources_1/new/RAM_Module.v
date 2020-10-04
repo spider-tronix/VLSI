b2cd9bb6f76134ca1facb11fd7ffbcd23ae11f56
akilm <akilmv555@gmail.com>
2020-10-05 04:43:16 +0530

    Modified according to .v standards

--- RV32IM/RV32IM.srcs/sources_1/new/RAM_Module.v
+++ RV32IM/RV32IM.srcs/sources_1/new/RAM_Module.v
@@ -1,15 +1,16 @@
 `timescale 1ns / 1ps
 
-module RAM_Module( Addr,Offset_Imm, Data_In, Data_Out, clk, Rd_En , Wt_En , RAM_Enable );
-parameter WORDLENGTH = 8;
-parameter XLEN = 32;
-parameter IMMEDIATE = 12;
-parameter Size = 2048;               //2**61 memory words , Defined as per David patterson Computer Architecture.
-input [XLEN-1:0] Addr;
-input [IMMEDIATE-1:0] Offset_Imm;
-input [XLEN-1:0] Data_In;
-input wire Rd_En,Wt_En,RAM_Enable,clk;
-output reg [XLEN-1:0] Data_Out;
+module RAM_Module #(parameter WORDLENGTH = 8,
+                    parameter XLEN = 32,
+                    parameter IMMEDIATE = 12,
+                    parameter Size = 2048)
+                    
+                   (input [XLEN-1:0] Addr,
+                    input [IMMEDIATE-1:0] Offset_Imm, 
+                    input [XLEN-1:0] Data_In,
+                    input Rd_En,Wt_En,RAM_Enable,clk,
+                    output reg [XLEN-1:0] Data_Out);
+                     
 reg [WORDLENGTH-1:0] RAM_mem[0:Size-1];
 
 always @(posedge clk)
