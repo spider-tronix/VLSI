51432cd8194172c885eafa688b8bc74f6e0dcfeb
akilm <akilmv555@gmail.com>
2020-10-18 21:37:40 +0530

    Decode Stage for Jump and Branch updated

--- RV32IM/RV32IM.srcs/sources_1/imports/new/Decoder.v
+++ RV32IM/RV32IM.srcs/sources_1/imports/new/Decoder.v
@@ -36,23 +36,68 @@ begin
         opcode <= IR[6:0];
         case(opcode)
         `OP_OP:
-               begin
+                begin
                     src1 <= IR[19:15];
                     src2 <= IR[24:20];
                     dest <= IR[11:7];
                     funct3 <= IR[14:12]; 
                     funct7 <= IR[31:25];
                     imm <= 'bz;
-               end
+                end
         `OP_OP_IMM:
-               begin
+                begin
                     src1 <= IR[19:15];
                     src2 <= 'bz;
                     dest <= IR[11:7];
                     funct3 <= IR[14:12]; 
                     funct7 <= 'bz;
                     imm <= {20*{IR[31]},IR[31:20]};
-               end
+                end
+        `OP_LUI:
+                begin
+                    src1 <= 'bz;
+                    src2 <= 'bz;
+                    dest <= IR[11:7];
+                    funct3 <= 'bz;
+                    funct7 <= 'bz;
+                    imm <= {IR[31:12],12*{0}};
+                end
+        `OP_AUIPC:
+                begin
+                    src1 <= 'bz;
+                    src2 <= 'bz;
+                    dest <= IR[11:7];
+                    funct3 <= 'bz;
+                    funct7 <= 'bz;
+                    imm <= {IR[31:12],12*{0}};
+                end
+        `OP_JAL:
+                begin
+                    src1 <= 'bz;
+                    src2 <= 'bz;
+                    dest <= IR[11:7];
+                    funct3 <= 'bz;
+                    funct7 <= 'bz;
+                    imm <= {20*IR[31],IR[20],IR[30:21],0};
+                end  
+        `OP_JALR:
+                begin
+                    src1 <= IR[19:15];
+                    src2 <= 'bz;
+                    dest <= IR[11:7];
+                    funct3 <= IR[14:12];
+                    funct7 <= 'bz;
+                    imm <= IR[31:20];
+                end
+        `OP_BRANCH:
+                begin
+                    src1 <= IR[19:15];
+                    src2 <= IR[24:20];
+                    dest <= 'bz;
+                    funct3 <= IR[14:12];
+                    funct7 <= 'bz;
+                    imm <= {20*IR[31],IR[7],IR[30:25],IR[11:8],0};
+                end
         default : ;
         endcase 
     end
