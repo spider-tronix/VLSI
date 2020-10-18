ef7bef1b6784f442a6419dc256a648ea50598a60
sudharsan2000 <ssudharsan2000@gmail.com>
2020-10-18 14:57:10 +0530

    Deleted all project files. Added only nesscesary

--- RV32IM/RV32IM.srcs/sources_1/imports/new/ROM_Module.v
+++ RV32IM/RV32IM.srcs/sources_1/imports/new/ROM_Module.v
@@ -8,17 +8,6 @@ Specifications : Word size = 8 bits
 */
 `timescale 1ns / 1ps
 
-<<<<<<< HEAD
-module ROM_Module #(parameter WORD_LENGTH = 8,
-                    parameter ROM_DEPTH = 2048,
-                    parameter XLEN = 32)
-                    
-                   (output reg [XLEN-1:0] Instr,
-                    input [XLEN-1:0] Addr,
-                    input wire ROM_Enable,clk,ROM_Rst);
-                    
-reg [WORD_LENGTH-1:0] ROM_mem[0:ROM_DEPTH-1]; 
-=======
 module ROM_Module(Addr,Instr,ROM_Enable,clk,ROM_Rst);
 parameter WORD_LENGTH = 8;
 parameter ROM_DEPTH = 2048;
@@ -27,7 +16,6 @@ output reg [XLEN-1:0] Instr;
 input [XLEN-1:0] Addr;
 input wire ROM_Enable,clk,ROM_Rst;
 reg [WORD_LENGTH-1:0] ROM_mem[0:ROM_DEPTH-1];                
->>>>>>> ALU_Block
 integer i;
 
 //Rom Write Block
