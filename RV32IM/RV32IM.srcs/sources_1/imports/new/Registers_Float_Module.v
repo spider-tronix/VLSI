70f10e6961331685468fa949e61c043a4053fa93
sudharsan2000 <ssudharsan2000@gmail.com>
2021-02-01 21:41:56 +0530

    Floating point reg

---
+++ RV32IM/RV32IM.srcs/sources_1/imports/new/Registers_Float_Module.v
@@ -0,0 +1,65 @@
+///////////////////////////////////////////////////////
+/*
+Module Name: Registers_Module
+Description : The internal 32 registers described in the RISC V Architecture. 
+              
+              Read Mode : two registers can be read at the same time, Index 
+              specified by ind_1 and ind_2 and output stored in D_out_1 and
+              D_out_2 if Reg_Rd_En is HIGH 
+              
+              Write Mode: single register can be written by specifying the
+              first index ind_1 (ind_2 is ignored).The value from D_in is 
+              stored in the register specified by the index
+              
+              Note: x0 is hard-wired to 0 as per the RISC-V architecture 
+Specifications : Word size = 32 bits
+                 
+*/
+`timescale 1ns / 1ps
+
+module Registers_Float_Module #(parameter FLEN = 32,
+                          parameter RegBank_Size = 32)
+                         
+                         (input [4:0] src1,src2, src3, dest,
+                          input wire re,re1,re2,we,
+                          // clk,
+                          input [FLEN-1:0] rd,
+                          output reg [FLEN-1:0] rs1,rs2,rs3);
+                          
+reg [FLEN-1:0] x[0:RegBank_Size-1];
+integer i;
+initial 
+begin
+x[0] <= 0; //register 0 hardwired to zero
+for(i=1;i<RegBank_Size;i=i+1)
+    begin 
+        if(i==2)
+        begin
+        x[i] <= 32'h000000ff;
+        end
+        else
+        begin
+        x[i] <= i;
+        end
+    end
+end
+
+always @(*)
+begin
+ if(re)
+    begin
+      rs1 <= re1 ? x[src1] : 'bz;
+      rs2 <= re2 ? x[src2] : 'bz;
+      rs3 <= re2 ? x[src3] : 'bz;
+    end
+end
+
+always @(*)
+begin
+ if(we)
+    begin
+      x[dest] <= dest ? rd[31:0] : 32'b0 ;
+    end
+end
+
+endmodule
