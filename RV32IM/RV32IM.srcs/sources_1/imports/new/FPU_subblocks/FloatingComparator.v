cf9aee00cdbb70a235a2899b56a21bb2262ec39b
sudharsan2000 <ssudharsan2000@gmail.com>
2021-03-19 19:06:58 +0530

    Reorganize files

--- RV32IM/RV32IM.srcs/sources_1/imports/new/FPU_subblocks/FloatingComparator.v
+++ RV32IM/RV32IM.srcs/sources_1/imports/new/FPU_subblocks/FloatingComparator.v
@@ -32,10 +32,27 @@
 module FloatingComparator #(parameter FLEN= 32, parameter NaN = 32'h7fc00000)(input enable, input [FLEN-1:0] A, input [FLEN-1:0]B, 
                         output reg [2:0] result_comp);
 
+reg SNAN_A = 0,QNAN_A = 0, SNAN_B=0, QNAN_B=0;
+reg[FLEN-1:0] A_NaN, B_NaN;
 always@(*)
 begin
+    A_NaN <=0;
+    B_NaN <=0;
+    //Checking for both signalling and quiet NaN and assigning it as NaN = 32'h7fc00000
+    SNAN_A <= (32'h7F800001<=A<= 32'h7FBFFFFF || 32'hFF800001<= A<= 32'hFFBFFFFF);
+    QNAN_A <= (32'h7FC00000<=A<=32'h7FFFFFFF || 32'hFFC00000<=A<=32'hFFFFFFFF);
+    SNAN_B <= (32'h7F800001<=B<= 32'h7FBFFFFF || 32'hFF800001<= B<= 32'hFFBFFFFF);
+    QNAN_B <= (32'h7FC00000<=B<=32'h7FFFFFFF || 32'hFFC00000<=B<=32'hFFFFFFFF);
+    
+    if(SNAN_A == 1 || QNAN_A ==1) 
+        A_NaN <= `NaN;
+        
+    if(SNAN_B == 1 || QNAN_B ==1) 
+        B_NaN <= `NaN;
+     // comparison cases   
     if (enable)begin 
-        if(A==B)begin
+        if(A==B && A_NaN!=`NaN && B_NaN!= `NaN)
+        begin
         result_comp <= 'b00;
         end
         
@@ -44,14 +61,14 @@ begin
     if(A[31] == B[31])begin
         result_comp = (A[30:23]>B[30:23])?'b010:'b000;
         if(A[30:23] == B[30:23])begin
-        result_comp = (A[22:0] > B[22:0] )? 'b010 : 'b000 ;
+        result_comp = (A[22:0] > B[22:0] )? 'b010 :'b000 ;
         end    
-       end
-    if(A == NaN && B != NaN)
+       end   
+    if(A_NaN == `NaN && B_NaN != `NaN)
         result_comp = 'b101; // result = B
-    else if(A!= NaN && B == NaN) 
+    else if(A_NaN!= `NaN && B_NaN == `NaN) 
         result_comp = 'b110; //result = A;
-    else if(A==NaN && B== NaN)
+    else if(A_NaN==`NaN && B_NaN == `NaN)
         result_comp = 'b111; //result = NaN;
     end
 end
