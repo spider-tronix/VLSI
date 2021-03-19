814642ef347506f32080484ed40185a2daaa7fb8
sudharsan2000 <ssudharsan2000@gmail.com>
2021-03-19 20:06:40 +0530

    Remove clocks FPU div

--- RV32IM/RV32IM.srcs/sources_1/imports/new/FPU_subblocks/FloatingDivision.v
+++ RV32IM/RV32IM.srcs/sources_1/imports/new/FPU_subblocks/FloatingDivision.v
@@ -26,29 +26,29 @@ reg [XLEN-1:0] B_scaled;
 reg en1,en2,en3,en4,en5;
 reg dummy;
 /*----Initial value----*/
-FloatingMultiplication M1(.A({{1'b0,8'd126,B[22:0]}}),.B(32'h3ff0f0f1),.clk(clk),.result(temp1), .enable(enable)); //verified
+FloatingMultiplication M1(.A({{1'b0,8'd126,B[22:0]}}),.B(32'h3ff0f0f1), .result(temp1), .enable(enable)); //verified
 assign debug = {1'b1,temp1[30:0]};
 FloatingAddition A1(.subract_A(0), .subract_B(0), .A(32'h4034b4b5),.B({1'b1,temp1[30:0]}),.result(x0), .enable(enable));
 
 /*----First Iteration----*/
-FloatingMultiplication M2(.A({{1'b0,8'd126,B[22:0]}}),.B(x0),.clk(clk),.result(temp2), .enable(enable));
+FloatingMultiplication M2(.A({{1'b0,8'd126,B[22:0]}}),.B(x0), .result(temp2), .enable(enable));
 FloatingAddition A2(.subract_A(0), .subract_B(0), .A(32'h40000000),.B({!temp2[31],temp2[30:0]}),.result(temp3), .enable(enable));
-FloatingMultiplication M3(.A(x0),.B(temp3),.clk(clk),.result(x1), .enable(enable));
+FloatingMultiplication M3(.A(x0),.B(temp3), .result(x1), .enable(enable));
 
 /*----Second Iteration----*/
-FloatingMultiplication M4(.A({1'b0,8'd126,B[22:0]}),.B(x1),.clk(clk),.result(temp4), .enable(enable));
+FloatingMultiplication M4(.A({1'b0,8'd126,B[22:0]}),.B(x1), .result(temp4), .enable(enable));
 FloatingAddition A3(.subract_A(0), .subract_B(0), .A(32'h40000000),.B({!temp4[31],temp4[30:0]}),.result(temp5), .enable(enable));
-FloatingMultiplication M5(.A(x1),.B(temp5),.clk(clk),.result(x2), .enable(enable));
+FloatingMultiplication M5(.A(x1),.B(temp5), .result(x2), .enable(enable));
 
 /*----Third Iteration----*/
-FloatingMultiplication M6(.A({1'b0,8'd126,B[22:0]}),.B(x2),.clk(clk),.result(temp6), .enable(enable));
+FloatingMultiplication M6(.A({1'b0,8'd126,B[22:0]}),.B(x2), .result(temp6), .enable(enable));
 FloatingAddition A4(.subract_A(0), .subract_B(0), .A(32'h40000000),.B({!temp6[31],temp6[30:0]}),.result(temp7), .enable(enable));
-FloatingMultiplication M7(.A(x2),.B(temp7),.clk(clk),.result(x3), .enable(enable));
+FloatingMultiplication M7(.A(x2),.B(temp7), .result(x3), .enable(enable));
 
 /*----Reciprocal : 1/B----*/
 assign Exponent = x3[30:23]+8'd126-B[30:23];
 assign reciprocal = {B[31],Exponent,x3[22:0]};
 
 /*----Multiplication A*1/B----*/
-FloatingMultiplication M8(.A(A),.B(reciprocal),.clk(clk),.result(result), .enable(enable));
+FloatingMultiplication M8(.A(A),.B(reciprocal), .result(result), .enable(enable));
 endmodule
